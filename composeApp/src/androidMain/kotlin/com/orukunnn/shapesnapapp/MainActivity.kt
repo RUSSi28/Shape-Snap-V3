cc492df2fc00921861140aa4e7cbc877557ae481
RUSSi28 <tktkk0228@outlook.jp>
2026-05-23 09:20:57 +0900

    ステータスバーとナビゲーションバーの色を黒に指定

--- composeApp/src/androidMain/kotlin/com/orukunnn/shapesnapapp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/orukunnn/shapesnapapp/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.orukunnn.shapesnapapp
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.orukunnn.shapesnapapp.app.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(Color.BLACK),
+            navigationBarStyle = SystemBarStyle.dark(Color.BLACK),
+        )
         super.onCreate(savedInstanceState)
 
         setContent {
